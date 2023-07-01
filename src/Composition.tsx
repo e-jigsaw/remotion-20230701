88ba4a110382be9cbcea1b66e29a5ffdd79483fb
jigsaw <m@jgs.me>
2023-07-01 20:16:43 +0900

    go

--- src/Composition.tsx
+++ src/Composition.tsx
@@ -1,3 +1,14 @@
+import { Video, staticFile } from 'remotion';
+import { Cc } from './CC';
+
 export const MyComposition = () => {
-  return null;
+  return (
+    <>
+      <Video
+        src={staticFile('v.mp4')}
+        style={{ margin: '0 auto', width: 1280, height: 720 }}
+      />
+      <Cc />
+    </>
+  );
 };
